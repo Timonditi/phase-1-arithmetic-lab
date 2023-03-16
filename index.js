3d771e04b138cb786fde8703ea0963876dcc3562
Timothy <todongoonditi@gmail.com>
2023-03-16 21:19:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2
+
+
+let random = Math.ceil(Math.random() * 100);
+
+
+let num3 = 16;
+let num4 = 6;
+
+
+let mod = num3 % num4;
+
+let numbers = [5, 10, 15, 20, ];
+
+let max = Math.max(...numbers);
+
+
+  
